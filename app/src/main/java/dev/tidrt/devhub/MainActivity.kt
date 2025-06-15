8ac3c14e821f3ddbe550f851a60c49b10c3d4e85
tiago <dev.tidrt@gmail.com>
2025-06-15 15:32:00 -0300

    second day challenge of 7DoC Alura:Adroid using jetpack compose

--- app/src/main/java/dev/tidrt/devhub/MainActivity.kt
+++ app/src/main/java/dev/tidrt/devhub/MainActivity.kt
@@ -1,20 +1,40 @@
 package dev.tidrt.devhub
 
+import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.tidrt.devhub.ui.theme.DevHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,24 +54,55 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun App(modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_profile),
-            contentDescription = null
+        val boxHeight = remember {
+            100.dp
+        }
+        val imageHeight = remember {
+            boxHeight
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(
+                    RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp)
+                )
+                .background(
+                    Color.Gray,
+                )
+                .height(boxHeight),
         )
-        Text(
-            text = "Tiago Duarte",
-            style = MaterialTheme.typography.titleMedium
-        )
-        Text(
-            text = "tidrt",
-            style = MaterialTheme.typography.bodyMedium
-        )
-        Text(
-            text = "Sobre você...",
-            style = MaterialTheme.typography.bodyMedium
-        )
+        Box(
+            modifier = Modifier.graphicsLayer {
+               translationY = -150F
+            }
+        ){
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .height(imageHeight),
+                    painter = painterResource(id = R.drawable.ic_profile),
+                    contentDescription = null
+                )
+                Text(
+                    text = "Tiago Duarte",
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Text(
+                    text = "tidrt",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Text(
+                    text = "Bio do Github",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
     }
 }
 
